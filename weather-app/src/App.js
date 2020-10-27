2e362b7a600be935bd5a767bd4d36f5103d7c9b1
KhusniddinovUz <khusniddinovuz@gmail.com>
2020-10-27 20:55:16 +0500

    Send request by user search

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -6,15 +6,35 @@ import './index.scss';
 
 class App extends Component {
   state = {
-    search: '',
+    data: {},
     type: sunny,
   };
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const value = document.getElementById('search').value;
+    const data = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${value}&appid=2f96ebd33fbe89fdc545dcfef0fe55b6`
+    );
+    const response = await data.json();
+    const type = response['weather'][0]['main'];
+    document.getElementById('search').value = '';
+    this.setState({ data: response });
+    if (type === 'Clouds' || type === 'Mist' || type === 'Smoke') {
+      this.setState({ type: cloudy });
+    } else if (type === 'Raining') {
+      this.setState({ type: raining });
+    } else {
+      this.setState({ type: sunny });
+    }
+  };
+
   render() {
     return (
       <div className='App'>
         <img className='img' alt='weather' src={this.state.type} />
         <div>
-          <form className='form'>
+          <form className='form' onSubmit={(e) => this.getWeather(e)}>
             <input type='text' id='search' />
             <button type='submit'>Search</button>
           </form>
