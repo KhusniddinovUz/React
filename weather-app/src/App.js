7643fee3b4203cf83f14eb28647c45332f66d0f4
KhusniddinovUz <khusniddinovuz@gmail.com>
2020-10-27 17:44:35 +0500

    Add styling and node-sass

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import sunny from './media/sunny.jpg';
 import raining from './media/raining.jpg';
 import cloudy from './media/cloudy.jpg';
+import './index.scss';
 
 class App extends Component {
   state = {
@@ -11,11 +12,13 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <form className='form'>
-          <input type='text' id='search' />
-          <button type='submit'>Search</button>
-        </form>
         <img className='img' alt='weather' src={this.state.type} />
+        <div>
+          <form className='form'>
+            <input type='text' id='search' />
+            <button type='submit'>Search</button>
+          </form>
+        </div>
       </div>
     );
   }
