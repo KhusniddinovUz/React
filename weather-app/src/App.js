5aabad1035e9daef85a6eb998f96b58639cb267e
KhusniddinovUz <khusniddinovuz@gmail.com>
2020-10-27 21:34:13 +0500

    Done with the project))

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -6,26 +6,30 @@ import './index.scss';
 
 class App extends Component {
   state = {
-    data: {},
+    data: '',
     type: sunny,
   };
 
   getWeather = async (e) => {
     e.preventDefault();
     const value = document.getElementById('search').value;
-    const data = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${value}&appid=2f96ebd33fbe89fdc545dcfef0fe55b6`
-    );
-    const response = await data.json();
-    const type = response['weather'][0]['main'];
-    document.getElementById('search').value = '';
-    this.setState({ data: response });
-    if (type === 'Clouds' || type === 'Mist' || type === 'Smoke') {
-      this.setState({ type: cloudy });
-    } else if (type === 'Raining') {
-      this.setState({ type: raining });
-    } else {
-      this.setState({ type: sunny });
+    if (value !== '') {
+      const data = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${value}&appid=2f96ebd33fbe89fdc545dcfef0fe55b6`
+      );
+      const response = await data.json();
+      if (response['cod'] !== '404') {
+        const type = response['weather'][0]['main'];
+        document.getElementById('search').value = '';
+        this.setState({ data: response });
+        if (type === 'Clouds' || type === 'Mist' || type === 'Smoke') {
+          this.setState({ type: cloudy });
+        } else if (type === 'Rain') {
+          this.setState({ type: raining });
+        } else {
+          this.setState({ type: sunny });
+        }
+      }
     }
   };
 
@@ -33,11 +37,30 @@ class App extends Component {
     return (
       <div className='App'>
         <img className='img' alt='weather' src={this.state.type} />
-        <div>
+        <div className='wrapper'>
           <form className='form' onSubmit={(e) => this.getWeather(e)}>
-            <input type='text' id='search' />
+            <input type='text' id='search' placeholder='Search for city...' />
             <button type='submit'>Search</button>
           </form>
+          {this.state.data !== '' && (
+            <div className='more'>
+              <ul>
+                <li>City</li>
+                <li>Country</li>
+                <li>Temperature</li>
+                <li>Weather</li>
+              </ul>
+              <ul>
+                <li>{this.state.data['name']}</li>
+                <li>{this.state.data['sys']['country']}</li>
+                <li>
+                  {`${parseInt(this.state.data['main']['temp'] - 273)}`}
+                  &#x2103;
+                </li>
+                <li>{this.state.data['weather'][0]['main']}</li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     );
