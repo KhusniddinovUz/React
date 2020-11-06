0161612b22b7b1742a8eaca519e27f127d276b2d
KhusniddinovUz <khusniddinovuz@gmail.com>
2020-11-06 14:43:49 +0500

    Create Register Form using Semantic UI

--- slack-clone/src/index.js
+++ slack-clone/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'semantic-ui-css/semantic.min.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
