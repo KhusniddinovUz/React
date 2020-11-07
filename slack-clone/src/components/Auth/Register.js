bfd61b8fa18a83ae4e1572e0c25e71c09790b2b8
KhusniddinovUz <khusniddinovuz@gmail.com>
2020-11-07 13:42:46 +0500

    Handling Errors on submit

--- slack-clone/src/components/Auth/Register.js
+++ slack-clone/src/components/Auth/Register.js
@@ -17,8 +17,47 @@ class Register extends Component {
     email: '',
     password: '',
     passwordConfirmation: '',
+    errors: [],
   };
 
+  isFormValid = () => {
+    let errors = [];
+    let error;
+
+    if (this.isFormEmpty(this.state)) {
+      error = { message: 'Fill in all fields' };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else if (!this.isPasswordValid(this.state)) {
+      error = { message: 'Password is invalid' };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+    return (
+      !username.length ||
+      !email.length ||
+      !password.length ||
+      !passwordConfirmation.length
+    );
+  };
+
+  isPasswordValid = ({ password, passwordConfirmation }) => {
+    if (password.length < 6 || passwordConfirmation.length < 6) {
+      return false;
+    } else if (password !== passwordConfirmation) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  displayErrors = (errors) =>
+    errors.map((error, i) => <p key={i}>{error.message}</p>);
   handleChange = (event) => {
     this.setState({
       [event.target.name]: event.target.value,
@@ -26,16 +65,24 @@ class Register extends Component {
   };
 
   handleSubmit = (event) => {
-    event.preventDefault();
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then((user) => console.log(user))
-      .catch((err) => console.log(err));
+    if (this.isFormValid()) {
+      event.preventDefault();
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then((user) => console.log(user))
+        .catch((err) => console.log(err));
+    }
   };
 
   render() {
-    const { username, email, password, passwordConfirmation } = this.state;
+    const {
+      username,
+      email,
+      password,
+      passwordConfirmation,
+      errors,
+    } = this.state;
 
     return (
       <Grid textAlign='center' verticalAlign='middle' className='app'>
@@ -92,6 +139,12 @@ class Register extends Component {
               </Button>
             </Segment>
           </Form>
+          {this.state.errors.length > 0 && (
+            <Message error>
+              <h3>Error</h3>
+              {this.displayErrors(this.state.errors)}
+            </Message>
+          )}
           <Message>
             Already a user? <Link to='/login'>Login</Link>
           </Message>
