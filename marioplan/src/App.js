96cc8c544f661a8b00fceed97b07348134622fa2
KhusniddinovUz <khusniddinovuz@gmail.com>
2020-12-15 17:19:11 +0500

    Create Dashboar, ProjectList and Notifications

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom'
-import Navbar from './components/layout/Navbar'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './components/dashboard/Dashboard';
+import Navbar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
+        <div className='App'>
           <Navbar />
+          <Switch>
+            <Route path='/' component={Dashboard} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
