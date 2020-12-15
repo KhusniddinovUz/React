afae165c96c699796ef525dd4b671381cc76582a
KhusniddinovUz <khusniddinovuz@gmail.com>
2020-12-15 17:05:10 +0500

    Create layout components

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <div className='App'>
-      <h1>MarioPlan</h1>
-    </div>
-  );
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
