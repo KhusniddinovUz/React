cc410edb690bca7ccfb98069ab564c7775d8b60b
KhusniddinovUz <khusniddinovuz@gmail.com>
2020-12-21 18:08:55 +0500

    Create Project component

---
+++ marioplan/src/components/projects/CreateProject.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+const CreateProject = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleChange = (e) => {
+    if (e.target.id === 'title') {
+      setTitle(e.target.value);
+    } else {
+      setContent(e.target.value);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, content);
+  };
+  return (
+    <div className='container'>
+      <form onSubmit={handleSubmit} className='white'>
+        <h5 className='grey-text text-darken-3'>Create Project</h5>
+        <div className='input-field'>
+          <label htmlFor='title'>Title</label>
+          <input type='text' id='title' onChange={handleChange} />
+        </div>
+        <div className='input-field'>
+          <label htmlFor='content'>Project Content</label>
+          <textarea
+            id='content'
+            className='materialize-textarea'
+            onChange={handleChange}
+          ></textarea>
+        </div>
+        <div className='input-field'>
+          <button className='btn pink lighten-1'>Create</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateProject;
