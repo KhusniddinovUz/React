09abb8bf3f068b30356ade1d915ac2d3e1fd1f0d
KhusniddinovUz <khusniddinovuz@gmail.com>
2020-12-22 16:32:32 +0500

    Adding dummy data, connecting components with redux

--- marioplan/src/components/dashboard/Dashboard.js
+++ marioplan/src/components/dashboard/Dashboard.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ProjectList from '../projects/ProjectList';
 import Notifications from './Notifications';
+import { useSelector } from 'react-redux';
 
 function Dashboard() {
+  const projects = useSelector((state) => state.project.projects);
   return (
     <div className='dashboard container'>
       <div className='row'>
         <div className='col s12 m6'>
-          <ProjectList />
+          <ProjectList projects={projects} />
         </div>
         <div className='col s12 m5 offset-m1'>
           <Notifications />
--- marioplan/src/store/reducers/projectReducer.js
+++ marioplan/src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {};
+const initState = {
+  projects: [
+    { id: 1, title: 'Help me find peach', content: 'blah blah blah' },
+    { id: 2, title: 'Drink water', content: 'drink water water water' },
+    { id: 3, title: 'Egg hunt with Mirzo', content: 'vayy musreee' },
+  ],
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
