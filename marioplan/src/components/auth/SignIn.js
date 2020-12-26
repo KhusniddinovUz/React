707d39f3411e4a368e0c292c3c0b296975be7003
KhusniddinovUz <khusniddinovuz@gmail.com>
2020-12-26 14:54:50 +0500

    Logging users in

--- marioplan/src/components/auth/SignIn.js
+++ marioplan/src/components/auth/SignIn.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import { signIn } from '../../store/actions/authActions';
 class SignIn extends Component {
   state = {
     email: '',
@@ -12,9 +13,10 @@ class SignIn extends Component {
   };
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    this.props.signIn(this.state);
   };
   render() {
+    const { authError } = this.props;
     return (
       <div className='container'>
         <form className='white myBox' onSubmit={this.handleSubmit}>
@@ -29,6 +31,9 @@ class SignIn extends Component {
           </div>
           <div className='input-field'>
             <button className='btn pink lighten-1 z-depth-0'>Login</button>
+            <div className='red-text center'>
+              {authError ? authError : null}
+            </div>
           </div>
         </form>
       </div>
@@ -36,4 +41,16 @@ class SignIn extends Component {
   }
 }
 
-export default SignIn;
+const mapStateToProps = (state) => {
+  return {
+    authError: state.auth.authError,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signIn: (creds) => dispatch(signIn(creds)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
--- marioplan/src/components/layout/Navbar.js
+++ marioplan/src/components/layout/Navbar.js
@@ -1,18 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import SignedInLinks from './SignedInLinks'
-import SignedOutLinks from './SignedOutLinks'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SignedInLinks from './SignedInLinks';
+import SignedOutLinks from './SignedOutLinks';
+import { connect } from 'react-redux';
 
-const Navbar = () => {
+const Navbar = (props) => {
+  console.log(props.firebase);
   return (
-    <nav className="nav-wrapper grey darken-3">
-      <div className="container">
-        <Link to='/' className="brand-logo">MarioPlan</Link>
+    <nav className='nav-wrapper grey darken-3'>
+      <div className='container'>
+        <Link to='/' className='brand-logo'>
+          MarioPlan
+        </Link>
         <SignedInLinks />
         <SignedOutLinks />
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+const mapStateToProps = (state) => {
+  return {
+    firebase: state.firebase,
+  };
+};
+
+export default connect(mapStateToProps)(Navbar);
