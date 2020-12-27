8003f944830e51333779b1013f8ae2139bd74b6c
KhusniddinovUz <khusniddinovuz@gmail.com>
2020-12-27 21:23:01 +0500

    Formatting Date with moment

--- marioplan/src/components/projects/ProjectDetails.js
+++ marioplan/src/components/projects/ProjectDetails.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { Redirect } from 'react-router-dom';
 import { compose } from 'redux';
+import moment from 'moment';
 
 const ProjectDetails = (props) => {
   const { project, auth } = props;
@@ -20,7 +21,11 @@ const ProjectDetails = (props) => {
             <div>
               Posted by {project.authorFirstName} {project.authorLastName}
             </div>
-            <div>2nd September, 2am</div>
+            <div>
+              {moment(project.createdAt.toDate()).format(
+                'MMMM Do YYYY, h:mm:ss a'
+              )}
+            </div>
           </div>
         </div>
       </div>
--- marioplan/src/store/actions/projectActions.js
+++ marioplan/src/store/actions/projectActions.js
@@ -1,17 +1,22 @@
 export const createProject = (project) => {
-  return (dispatch, getState, {getFirestore}) => {
-    // make async call to database
+  return (dispatch, getState, { getFirestore }) => {
     const firestore = getFirestore();
-    firestore.collection('projects').add({
-      ...project,
-      authorFirstName: 'Net',
-      authorLastName: 'Ninja',
-      authorId: 12345,
-      createdAt: new Date()
-    }).then(() => {
-      dispatch({ type: 'CREATE_PROJECT_SUCCESS' });
-    }).catch(err => {
-      dispatch({ type: 'CREATE_PROJECT_ERROR' }, err);
-    });
-  }
+    const profile = getState().firebase.profile;
+    const authorId = getState().firebase.auth.uid;
+    firestore
+      .collection('projects')
+      .add({
+        ...project,
+        authorFirstName: profile.firstName,
+        authorLastName: profile.lastName,
+        authorId: authorId,
+        createdAt: new Date(),
+      })
+      .then(() => {
+        dispatch({ type: 'CREATE_PROJECT_SUCCESS' });
+      })
+      .catch((err) => {
+        dispatch({ type: 'CREATE_PROJECT_ERROR' }, err);
+      });
+  };
 };
