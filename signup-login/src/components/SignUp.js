f18337575e199f368abafaf8947cf2ae660baba0
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-01-10 14:54:26 +0500

    Handling signup form submit, getting data

--- signup-login/src/components/SignUp.js
+++ signup-login/src/components/SignUp.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+  const [user, setUser] = useState({
+    username: '',
+    email: '',
+    password1: '',
+    password2: '',
+  });
+  const [border, setBorder] = useState('form-control');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (passwordValid()) {
+      // Fetch API for signup
+    }
+  };
+
+  const changeHandler = (e) => {
+    const newuser = Object.assign({}, user, { [e.target.id]: e.target.value });
+    setUser(newuser);
+  };
+
+  const passwordValid = () => {
+    if (user.password1 === user.password2 && user.password1.length > 6) {
+      setBorder('border-danger form-control');
+      return true;
+    } else {
+      setBorder('form-control');
+      return false;
+    }
+  };
+
   return (
     <div className='container SignUp p-3'>
       <h1 className='mt-4 p-1 text-center'>Signup for user profile</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className='form-group p-2 '>
           <label htmlFor='username'>Username</label>
           <input
@@ -13,6 +43,7 @@ const SignUp = () => {
             className='form-control'
             id='username'
             placeholder='Enter Username'
+            onChange={changeHandler}
           />
         </div>
         <div className='form-group p-2 '>
@@ -23,24 +54,28 @@ const SignUp = () => {
             id='email'
             aria-describedby='emailHelp'
             placeholder='Enter email'
+            onChange={changeHandler}
           />
         </div>
         <div className='form-group p-2'>
           <label htmlFor='password1'>Password</label>
           <input
             type='password'
-            className='form-control'
+            className={border}
             id='password1'
             placeholder='Password'
+            onChange={changeHandler}
           />
+          <small>Must be 6 chars at least</small>
         </div>
         <div className='form-group p-2'>
           <label htmlFor='password2'>Repeat Password</label>
           <input
             type='password'
-            className='form-control'
+            className={border}
             id='password2'
             placeholder='Password'
+            onChange={changeHandler}
           />
         </div>
         <div className='container d-flex align-items-center mt-2'>
